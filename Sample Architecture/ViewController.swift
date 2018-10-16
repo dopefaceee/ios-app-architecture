1843cdb53e5416334aafc6decceadc84c0a4ba4b
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2018-10-16 14:08:39 +0700

    Initial Commit

---
+++ Sample Architecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample Architecture
+//
+//  Created by Fathony Teguh Irawan on 16/10/18.
+//  Copyright © 2018 Fathony Teguh Irawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
